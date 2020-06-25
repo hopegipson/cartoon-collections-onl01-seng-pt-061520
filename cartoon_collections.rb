2e15bde56c3fdde520c90969d9a4c48605867f0e
Hope Gipson <gipson.hope@gmail.com>
2020-06-25 03:01:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-    longword = array.any{|i| i.length > 4}
+    longword = array.find{|i| i.length > 4}
     if longword != nil
     true
     else
